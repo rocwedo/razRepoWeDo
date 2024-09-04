d7f34433321ca7422a2052e614235322a8517b38
rocwedo <rocwedo@gmail.com>
2024-09-04 16:26:01 +1200

    add new

--- wedoRazVoiceProj/src/com/wedo/init/WeDoBlocVoiceServiceApp.java
+++ wedoRazVoiceProj/src/com/wedo/init/WeDoBlocVoiceServiceApp.java
@@ -8,8 +8,8 @@ public class WeDoBlocVoiceServiceApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Start...");
-		app.dealwithRes("res\\raz\\raz new.txt","Raz level Z 2");
-//		app.dealwithRes("res\\raz02\\raz b 2023ext.txt","Raz level Z");
+		app.dealwithRes("res\\re3\\re3.txt","Reading Explorer Level 3");
+//		app.dealwithRes("res\\raz\\raz new.txt","Raz level Z 2");
 //		app.dealwithResFold("res\\raz02");
 		System.out.println("End");
 	}
